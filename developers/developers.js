3d5c663e1d967fb19746ca0d58b7f1e47963498b
vineet.dubey <vineet.dubey@zscaler.com>
2021-06-07 16:39:27 +0530

    Boilerplate for backend apis

--- developers/developers.js
+++ developers/developers.js
@@ -1,4 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const developersManager = require('./developersManager');
+
+router.get('/', (req, res) => {
+  res.send('Get all developers');
+});
+
+router.post('/', (req, res) => {
+  res.send('Add a developer');
+});
+
+router.get('/:id', (req, res) => {
+  res.send('Get a developer');
+});
+
+router.delete('/:id', (req, res) => {
+  res.send('Remove a developer');
+});
 
 module.exports = router;
