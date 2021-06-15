136b779f02260529e1bcb2ecdeeca83c0e09d0f8
vineet.dubey <vineet.dubey@zscaler.com>
2021-06-15 16:59:50 +0530

    Updated for demo

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -1,8 +1,36 @@
 import './ProfilePage.css';
-import Footer from './Footer';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function ProfilePage() {
-  return <Footer />;
+class ProfilePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: {} };
+  }
+
+  fetchDeveloperDetails = (id) => {
+    fetch(`/api/developers/${id}`)
+      .then((response) => response.json())
+      .then((devData) => {
+        this.setState({ data: devData });
+      });
+  };
+
+  componentDidMount() {
+    this.fetchDeveloperDetails(this.props.match.params.id);
+  }
+
+  render() {
+    return <pre>{JSON.stringify(this.state.data, null, '\t')}</pre>;
+  }
 }
+
+ProfilePage.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default ProfilePage;
