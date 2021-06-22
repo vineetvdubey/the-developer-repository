a0c15aabaa52de836b3f5b1a86450dbe44b2767b
vineetvdubey <35378726+vineetvdubey@users.noreply.github.com>
2021-06-22 21:44:59 +0530

    Made profile page responsive

--- client/src/components/ProfilePage.js
+++ client/src/components/ProfilePage.js
@@ -22,7 +22,90 @@ class ProfilePage extends React.Component {
   }
 
   componentDidMount() {
-    this.fetchDeveloperDetails(this.props.match.params.id);
+    this.setState({
+      data: {
+        id: 'vineetvdubey',
+        avatar_url: 'https://avatars.githubusercontent.com/u/35378726?v=4',
+        name: 'Vineet Dubey',
+        company: 'Kuliza Technologies',
+        blog: 'https://vineetvdubey.netlify.app/',
+        location: 'India',
+        email: '',
+        bio: 'Software Engineer, workaholic, gamer !!',
+        github_id: 'vineetvdubey',
+        linkedin_id: 'vineetvdubey',
+        codechef_id: 'vineetvdubey',
+        hackerrank_id: 'vineetvdubey',
+        twitter_id: 'vineetvdubey',
+        medium_id: 'vineetvdubey',
+        repos: [
+          {
+            name: 'chota-url',
+            html_url: 'https://github.com/vineetvdubey/chota-url',
+            description:
+              'A URL shortening app with backend in NodeJS + express.js and frontend with vanilla html/css/js',
+            updated_at: '2021-06-05T17:04:14Z',
+          },
+          {
+            name: 'htmlcss-component-library',
+            html_url: 'https://github.com/vineetvdubey/htmlcss-component-library',
+            description: 'An HTML/CSS project covering basic concepts.',
+            updated_at: '2021-05-22T11:02:17Z',
+          },
+          {
+            name: 'htmlcss-personal-resume',
+            html_url: 'https://github.com/vineetvdubey/htmlcss-personal-resume',
+            description: 'Personal resume website built using vanilla HTML/CSS',
+            updated_at: '2021-05-22T12:07:26Z',
+          },
+          {
+            name: 'QuizAppJSCli',
+            html_url: 'https://github.com/vineetvdubey/QuizAppJSCli',
+            description: 'A command-line based quiz application written in JS.',
+            updated_at: '2021-05-02T05:39:15Z',
+          },
+          {
+            name: 'SimpleSearch',
+            html_url: 'https://github.com/vineetvdubey/SimpleSearch',
+            description: 'A simple indexing exercise.',
+            updated_at: '2019-08-20T18:24:23Z',
+          },
+          {
+            name: 'the-developer-repository',
+            html_url: 'https://github.com/vineetvdubey/the-developer-repository',
+            description:
+              'A platform where anyone can create their developer profile. Created using NodeJS backend + ReactJS frontend.',
+            updated_at: '2021-06-21T14:44:53Z',
+          },
+          {
+            name: 'utility-apps-ui',
+            html_url: 'https://github.com/vineetvdubey/utility-apps-ui',
+            description:
+              'A simple website using vanilla HTML/CSS/JS implementing various utility tools and converters.',
+            updated_at: '2021-05-31T08:53:22Z',
+          },
+          {
+            name: 'UtilityAppsJSCli',
+            html_url: 'https://github.com/vineetvdubey/UtilityAppsJSCli',
+            description: 'Multiple command-line based mini applications written in JS.',
+            updated_at: '2021-05-02T05:40:07Z',
+          },
+          {
+            name: 'vineetvdubey',
+            html_url: 'https://github.com/vineetvdubey/vineetvdubey',
+            description: 'Config files for my GitHub profile.',
+            updated_at: '2021-05-02T05:35:20Z',
+          },
+          {
+            name: 'vineetvdubey.github.io',
+            html_url: 'https://github.com/vineetvdubey/vineetvdubey.github.io',
+            description: 'Profile page',
+            updated_at: '2021-05-02T08:03:57Z',
+          },
+        ],
+      },
+    });
+    // this.fetchDeveloperDetails(this.props.match.params.id);
   }
 
   months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
